8b4117f464f82777359940a3342badc91f14b995
gagal18 <gagaleskibojan@gmail.com>
2024-05-19 23:28:01 +0200

    feat: lab_3/exe1

---
+++ lab_3/exe1/UDPServer.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UDPServer extends Thread {
+
+    private DatagramSocket socket;
+    private byte[] buffer;
+
+    public UDPServer(int port) {
+        try {
+            this.socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        this.buffer = new byte[256];
+    }
+
+    @Override
+    public void run() {
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    
+        while (true) {
+            try {
+                socket.receive(packet);
+                System.out.println(packet.getLength());
+                String received = new String(packet.getData(), 0, packet.getLength());
+                System.out.println("RECEIVED: " + received);
+                if(received.equals("login")) {
+                    received = "logged in";
+                }
+                if(received.equals("logout")) {
+                    received = "logged out";
+                }
+                byte[] sendData = received.getBytes();
+                
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
+                packet.setData(buffer);                
+                socket.send(sendPacket);
+                
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        UDPServer server = new UDPServer(4445);
+        server.start();
+    }
+}
